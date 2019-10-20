6cd8c9b418fc00d799f72c3e1e3f4a565c59ab1e
drowley8 <54674066+drowley8@users.noreply.github.com>
2019-10-20 16:49:08 -0600

    Add save()

--- player.py
+++ player.py
@@ -1,12 +1,11 @@
 import math        #import needed modules
 import pyaudio     #sudo apt-get install python-pyaudio
+import wave
 
-PyAudio = pyaudio.PyAudio     #initialize pyaudio
+p = pyaudio.PyAudio()   #initialize pyaudio
 
 def play(WAVEDATA, BITRATE):
-    print(WAVEDATA)
 
-    p = PyAudio()
     stream = p.open(format = p.get_format_from_width(1),
                     channels = 1,
                     rate = BITRATE,
@@ -16,3 +15,11 @@ def play(WAVEDATA, BITRATE):
     stream.stop_stream()
     stream.close()
     p.terminate()
+
+def save(WAVEDATA, BITRATE, CHANNELS):
+    waveFile = wave.open("file.wav", 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(p.get_sample_size(p.get_format_from_width(1)))
+    waveFile.setframerate(BITRATE)
+    waveFile.writeframes(WAVEDATA.encode())
+    waveFile.close()
