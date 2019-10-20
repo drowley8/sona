d670447a1a2432d7d21a507b6a85e308aa2242f6
drowley8 <54674066+drowley8@users.noreply.github.com>
2019-10-20 16:52:00 -0600

    Add rest()

--- waveforms.py
+++ waveforms.py
@@ -4,19 +4,27 @@ from player import play
 
 def sine(FREQUENCY, LENGTH, BITRATE, AMP):
     NUMBEROFFRAMES = int(BITRATE * LENGTH)
-
     WAVEDATA = ''
-
     for x in range(NUMBEROFFRAMES):
         WAVEDATA += chr(int(math.sin(2*x*FREQUENCY*math.pi/BITRATE)*AMP+64))
-
-    play(WAVEDATA, BITRATE)
+    print(WAVEDATA) # see the cyclic action
+    #play(WAVEDATA, BITRATE)
+    return WAVEDATA
 
 def noise(LENGTH, BITRATE):
     NUMBEROFFRAMES = int(BITRATE * LENGTH)
-
     WAVEDATA = ''
     for x in range(NUMBEROFFRAMES):
         WAVEDATA += chr(random.randint(0,127))
+    print(WAVEDATA) # see chaos
+    #play(WAVEDATA, BITRATE)
+    return WAVEDATA
 
-    play(WAVEDATA, BITRATE)
+def rest(LENGTH, BITRATE):
+    NUMBEROFFRAMES = int(BITRATE * LENGTH)
+    WAVEDATA = ''
+    for x in range(NUMBEROFFRAMES):
+        WAVEDATA += chr(64) # constant data for a constant silence
+    print(WAVEDATA) # see uniformity
+    #play(WAVEDATA, BITRATE)
+    return WAVEDATA
