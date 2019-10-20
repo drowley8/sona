6f5366223f0452a1651588e9c55b48ca42667004
drowley8 <54674066+drowley8@users.noreply.github.com>
2019-10-20 16:47:59 -0600

    Add song

--- gensound.py
+++ gensound.py
@@ -1,18 +1,38 @@
+import pyaudio
 import waveforms
-
+from player import play, save
 
 #See https://en.wikipedia.org/wiki/Bit_rate#Audio
-BITRATE = 384000 # on a scale of 8000-384000
+BITRATE = 44100 # on a scale of 8000-384000
+                # "Normal" would be 44100
 AMP = 63 # on a scale of 0-64
 FREQUENCY = 440 # human hearing is between 20-20000hz
 
+# We're running at 60 BPM!
+
 WAVEDATA = ''
-waveforms.noise(2, BITRATE)
-waveforms.sine(262, 1, BITRATE, 63)
-waveforms.sine(294, 1, BITRATE, 63)
-waveforms.sine(330, 1, BITRATE, 63)
-waveforms.sine(349, 1, BITRATE, 63)
-waveforms.sine(392, 2, BITRATE, 63)
+WAVEDATA += waveforms.noise(.5, BITRATE)
+WAVEDATA += waveforms.rest(.25, BITRATE)
+WAVEDATA += waveforms.sine(698, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(880, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(988, .5, BITRATE, 63)
+WAVEDATA += waveforms.sine(698, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(880, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(988, .5, BITRATE, 63)
+WAVEDATA += waveforms.sine(698, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(880, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(988, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(1318.5, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(1175, .5, BITRATE, 63)
+WAVEDATA += waveforms.sine(988, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(1046.5, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(988, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(784, .25, BITRATE, 63)
+WAVEDATA += waveforms.sine(659, 1, BITRATE, 63)
+WAVEDATA += waveforms.rest(.25, BITRATE)
+WAVEDATA += waveforms.noise(.5, BITRATE)
+play(WAVEDATA, BITRATE)
+save(WAVEDATA, BITRATE, 1)
 
 #for x in range(NUMBEROFFRAMES):
 #    WAVEDATA += chr(random.randint(32,96))
