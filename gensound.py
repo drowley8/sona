5c348533703e79b4a6baa580cc950c85c3d1a249
drowley8 <54674066+drowley8@users.noreply.github.com>
2019-10-21 11:12:54 -0600

    Update AMP

--- gensound.py
+++ gensound.py
@@ -5,13 +5,14 @@ from player import play, save
 #See https://en.wikipedia.org/wiki/Bit_rate#Audio
 BITRATE = 44100 # on a scale of 8000-384000
                 # "Normal" would be 44100
-AMP = 63 # on a scale of 0-64. Currently does nothing. Do not exceed 63
+AMP = 64 # on a scale of 0-128. Do not exceed 128
 FREQUENCY = 440 # human hearing is between 20-20000hz, so...
 
 # We're running at 60 BPM!
 
 WAVEDATA = '' # initialize the WAVEDATA!
               # I hoped that would have sounded cooler, but it didn't
+#WAVEDATA += waveforms.sine(440, .01, BITRATE, AMP)
 #WAVEDATA += waveforms.noise(.5, BITRATE)
 #WAVEDATA += waveforms.rest(.25, BITRATE)
 WAVEDATA += waveforms.sine(698, .25, BITRATE, AMP)
@@ -30,8 +31,8 @@ WAVEDATA += waveforms.sine(1046.5, .25, BITRATE, AMP)
 WAVEDATA += waveforms.sine(988, .25, BITRATE, AMP)
 WAVEDATA += waveforms.sine(784, .25, BITRATE, AMP)
 WAVEDATA += waveforms.sine(659, 1, BITRATE, AMP)
-#WAVEDATA += waveforms.rest(.25, BITRATE)
-#WAVEDATA += waveforms.noise(.5, BITRATE)
+WAVEDATA += waveforms.rest(.25, BITRATE)
+WAVEDATA += waveforms.noise(.5, BITRATE)
 play(WAVEDATA, BITRATE) # Drop needle here
 save(WAVEDATA, BITRATE, 1) # Extra channels = extra speed. For now.
 
