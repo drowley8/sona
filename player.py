39a6203f9e76a80ce0225b5ba000cb139bba1c4c
drowley8 <54674066+drowley8@users.noreply.github.com>
2019-10-21 08:39:44 -0600

    Edit player.py

--- player.py
+++ player.py
@@ -3,9 +3,11 @@ import pyaudio     # sound wave maker (sort of) and player
 import wave        # sound wave saver
 
 p = pyaudio.PyAudio()   #initialize pyaudio
+FORMAT = p.get_format_from_width(1, unsigned=True)
+#p.get_sample_size(p.get_format_from_width(1))
 
 def play(WAVEDATA, BITRATE):
-    stream = p.open(format = p.get_format_from_width(1),
+    stream = p.open(format = FORMAT,
                     channels = 1,
                     rate = BITRATE,
                     output = True)
@@ -17,7 +19,7 @@ def play(WAVEDATA, BITRATE):
 def save(WAVEDATA, BITRATE, CHANNELS):
     waveFile = wave.open("file.wav", 'wb')
     waveFile.setnchannels(CHANNELS) # Yes, but actually no.
-    waveFile.setsampwidth(p.get_sample_size(p.get_format_from_width(1))) # Heck
+    waveFile.setsampwidth(p.get_sample_size(FORMAT))
     waveFile.setframerate(BITRATE)
     waveFile.writeframes(WAVEDATA.encode())
     waveFile.close()
